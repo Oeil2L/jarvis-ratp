35f1f73659449fd46aa1b3624e560763a91dac13
Oeil2L <35351658+Oeil2L@users.noreply.github.com>
2018-04-10 21:53:56 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,3 +1,1 @@
-forecast_bus="https://api-ratp.pierre-grimaud.fr/v3/schedules/bus/258/les+fontenelles/R"
-forecast_train="https://api-ratp.pierre-grimaud.fr/v3/schedules/rers/a/nanterre+prefecture/A"
-forecast_train_direction="T"
+forecast_bus="https://api-ratp.pierre-grimaud.fr/v3/schedules/{type}/{code}/{station}/{way}"
