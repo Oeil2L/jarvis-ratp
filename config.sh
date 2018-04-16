3b52b9b9468457f7137fc0a741c3f5b70d85ac47
Oeil2L <35351658+Oeil2L@users.noreply.github.com>
2018-04-16 21:54:58 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,1 +1,22 @@
-forecast_bus="https://api-ratp.pierre-grimaud.fr/v3/schedules/{type}/{code}/{station}/{way}"
+#forecast_bus="https://api-ratp.pierre-grimaud.fr/v3/schedules/{type}/{code}/{station}/{way}"
+
+# ex bus, métro, noctilien ou tramway:
+#forecast_bus="https://api-ratp.pierre-grimaud.fr/v3/schedules/metros/1/les+sablons/A"
+#forecast_bus="https://api-ratp.pierre-grimaud.fr/v3/schedules/bus/PC1/porte+dauphine+++marechal+de+lattre+de+tassigny/A"
+#forecast_bus="https://api-ratp.pierre-grimaud.fr/v3/schedules/bus/52/capucines+++caumartin/A"
+#forecast_bus="https://api-ratp.pierre-grimaud.fr/v3/schedules/noctiliens/66/armorique+++musee+postal/R"
+#forecast_bus="https://api-ratp.pierre-grimaud.fr/v3/schedules/tramways/2/puteaux/R"
+#forecast_bus="https://api-ratp.pierre-grimaud.fr/v3/schedules/tramways/2/parc+pierre+lagravere/A"
+
+#ex rer A et B uniquement:
+#forecast_train="https://api-ratp.pierre-grimaud.fr/v3/schedules/rers/a/la+defense-grande+arche/R"
+#forecast_train="https://api-ratp.pierre-grimaud.fr/v3/schedules/rers/b/chatelet/R"
+#Préciser les directions acceptables: code train à 1 ou 2 lettres (séparateur ","), laisser vide pour toutes directions
+#forecast_train_direction=""
+#forecast_train_direction="J,S"
+
+
+forecast_bus=
+
+forecast_train=
+forecast_train_direction=
