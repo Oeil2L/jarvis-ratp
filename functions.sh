42a7ff304b896cc0aa625b9fe8fe3d3c0e51e0cf
Oeil2L <35351658+Oeil2L@users.noreply.github.com>
2018-04-16 21:57:06 +0200

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -1,27 +1,30 @@
-
 #!/bin/bash
-# Here you can create functions which will be available from the commands file
-# You can also use here user variables defined in your config file
-# To avoid conflicts, name your function like this
-# pg_XX_myfunction () { }
-# pg for PluGin
-# XX is a short code for your plugin, ex: ww for Weather Wunderground
-# You can use translations provided in the language folders functions.sh
 
 jv_pg_ratp_bus () {
-#    local day_num=${1:-0}
-    say "$(ww_lang i_check)"
+say "$(ww_lang i_check)"
 
-    local json="$(curl -s "$forecast_bus")"
-#    local json="$(curl -s "https://api-ratp.pierre-grimaud.fr/v3/schedules/bus/258/les+fontenelles/R")"
-# https://api-ratp.pierre-grimaud.fr/v3/schedules/bus/258/les+fontenelles/R
- #   $verbose && jv_debug "json: $json"
-#forecast="prochain busse $(echo "$json" | jq  ".result.schedules[0].message") le suivant $(echo "$json" | jq  ".result.schedules[1].message")"
-forecast="prochain busse $(echo "$json" | jq  ".result.schedules[0].message") le suivant $(echo "$json" | jq  ".result.schedules[1].message")"
-#local humanized =
+type="$(echo "$forecast_bus" | cut -d/ "-f6")"
+
+#boucle de mise au propre de $type
+case $type in
+"bus")
+type="busse";;
+"metros")
+type="métro";;
+esac
+
+local json="$(curl -s "$forecast_bus")"
+forecast="prochain "$type" $(echo "$json" | jq  ".result.schedules[0].message") le suivant $(echo "$json" | jq  ".result.schedules[1].message")"
 say "$(echo "$forecast" | sed "s/mn/minutes/g")"
-#say "$(echo "$forecast" | sed "s/mn/minutes/g" | sed "s/V\./voie /g" | sed "s/voie/voie minutes/g")"
-
-#echo $json
-#say "$forecast"
 }
+
+jv_pg_ratp_train () {
+say "$(ww_lang i_check)"
+
+local json="$(curl -s "$forecast_train")"
+
+direction="$(echo $forecast_train_direction | sed  -e 's/.*/\U&/' -e s/,/\"\),startswith\(\"/g -e 's/.*/startswith\(\"&/' -e 's/.*/&\"\)/')"
+forecast="prochain train $(echo "$json" | jq  "[[.result.schedules[] |  select ( .code|$direction)] | .[] | select (.message|contains(\":\"))]|.[$
+say "$(echo "$forecast" | sed "s/00:/minuit /g; s/12:/midi /g")"
+}
+
